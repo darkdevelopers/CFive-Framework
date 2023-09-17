3f0b8a8fdfaa83727e41baa122909054e0d7c518
Hörnchen <metyxs@gmail.com>
2023-09-17 23:05:15 +0200

    add Handlers directory and moved CarHandler in a own cs file.

---
+++ Client/Handlers/CommandsHandler.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using CitizenFX.Core;
+using static CitizenFX.Core.Native.API;
+
+namespace CFive_Framework.Client.Handlers
+{
+    public class CommandsHandler : BaseScript
+    {
+        public CommandsHandler()
+        {
+            EventHandlers.Add("onClientResourceStart", new Action<string>(OnClientResourceStart));
+        }
+
+        private static void OnClientResourceStart(string resourceName)
+        {
+            if (GetCurrentResourceName() != resourceName)
+                return;
+            
+            RegisterCommand("spawnVehicle",new Action<int, List<object>, string>(CarHandler.CreateVehicle), false);
+        }
+        
+    }
+    
+}
